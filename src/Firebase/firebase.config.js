a531397618dce99f7b877541c615a6208c32f3e3
souravhasan992 <souravhasan992@gmail.com>
2021-11-04 05:04:17 +0600

    configuration setup done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDdH8RYTCQZkTAG5zL7oXdaD5dBcQjrOY",
+    authDomain: "ema-john-simple-23389.firebaseapp.com",
+    projectId: "ema-john-simple-23389",
+    storageBucket: "ema-john-simple-23389.appspot.com",
+    messagingSenderId: "608397277607",
+    appId: "1:608397277607:web:431b5f1de2cdc987f5de82"
+};
+
+export default firebaseConfig;
